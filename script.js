cfe8cc9796037f3b65319ab7bca75dd41a5808d8
missopi <missopi@me.com>
2022-09-04 09:25:04 +0100

    Add clear function

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const currentOperation = document.querySelector('[data-current]');
 
 
 function operate() {
-    let calculation
-    let prev = this.previousOperation;
-    let current = this.currentOperation;
+    let calculation;
+    let prev = this.previousOperator;
+    let current = this.currentOperator;
     if (prev === NaN || current === NaN) return;
     if (this.operation === 'add') {
         calculation = prev + current;
@@ -20,9 +20,14 @@ function operate() {
         calculation = prev * current;
     } else if (this.operation === 'divide') {
         calculation = prev / current;
-    }
-    this.currentOperation = calculation;
+    };
+    this.currentOperator = calculation;
     this.operation = undefined;
-    this.previousOperation = '';
+    this.previousOperator = '';
 };
 
+function clear() {
+    this.currentOperator = ''
+    this.previousOperator = ''
+    this.operation = undefined
+};
